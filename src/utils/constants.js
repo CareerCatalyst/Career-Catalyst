791b68c59fe8bb852467302c5158222ed3758389
Fatima Mohamed <adropofilm@gmail.com>
2023-03-09 04:08:59 -0600

    Feat: add page details as constants

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const PATHS = {
+  ADVOCATING_FOR_YOURSELF: "advocating-for-yourself",
+  BUILDING_YOUR_NETWORK: "building-your-network",
+  CAREER_DEVELOPMENT: "career-development",
+  HEALTHCARE_PLANS: "healthcare-plans",
+  IMPOSTER_SYNDROME: "imposter-syndrome",
+  INTER_TIMEZONE_WORK: "inter-timezone-work",
+  NEGOTIATING_PAY: "negotiating-pay",
+  PERSONAL_BRAND: "personal-brand",
+  RESUMES: "resumes",
+  RETIREMENT_PLANS: "retirement-plans",
+  WORK_LIFE_BALANCE: "work-life-balance",
+  WORKPLACE_CULTURE: "workplace-culture",
+};
